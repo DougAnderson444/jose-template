74296fb383c4ef93c2e2ec63556f0bda0d90bd1b
Doug <douganderson444@gmail.com>
2021-02-28 19:10:25 -0500

    add cjs build

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -20,32 +20,16 @@ module.exports = [
     resolve: {
       mainFields: ['browser']
     }
+  },
+  {
+    entry,
+    mode,
+    output: {
+      filename: pkg.main,
+      path: path.resolve(__dirname, '')
+    },
+    resolve: {
+      mainFields: ['main']
+    }
   }
-  // {
-  //   entry,
-  //   mode,
-  //   output: {
-  //     filename: pkg.main,
-  //     path: path.resolve(__dirname, '')
-  //   },
-  //   resolve: {
-  //     mainFields: ['main']
-  //   }
-  // },
-  // {
-  //   entry,
-  //   mode,
-  //   experiments: {
-  //     outputModule: true
-  //   },
-  //   output: {
-  //     filename: pkg.module,
-  //     path: path.resolve(__dirname, ''),
-  //     module: true,
-  //     libraryTarget: 'module'
-  //   },
-  //   resolve: {
-  //     mainFields: ['module']
-  //   }
-  // }
 ]
