9fae1eeec683169c5bd91793d4e15762924f5ff1
Doug <douganderson444@gmail.com>
2021-02-28 19:10:41 -0500

    1.0.2

--- test/index.cjs.cjs
+++ test/index.cjs.cjs
@@ -4,7 +4,7 @@ If this library is written in es, it cannot be required by a commonjs script
  */
 
 const parseJWT = require('..')
-const tests = require('./basic.spec.mjs')
+const tests = require('./basic.spec.mjs') // doesn't work, can't require a es module
 const { expect } = require('chai')
 
 tests(expect, { parseJWT })
